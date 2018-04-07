b9e7134c56aec4d7a8dad3070e4e6e5c5f4138fd
tratda <david.trate@gmail.com>
2018-04-07 13:51:01 -0400

    Add support for user admin and create gir repo
    The user admin now exists with the password of "empiredidnothingwrong"
    Additionally the git repo now exists in the /home/admin directory

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,32 @@
 FROM python:3.5-slim
 
+RUN apt-get update
+
+RUN apt-get -y install git-core ssh
+
+RUN pip3 install --upgrade pip
+
+RUN useradd admin
+
+RUN mkdir -p /home/admin/admin
+
+RUN echo "admin:empiredidnothingwrong" | chpasswd
+
+RUN chown -R admin:admin /home/admin/
+
+WORKDIR /home/admin/admin/ 
+
+RUN su admin -c "git init --shared"
+
 WORKDIR /web
 
 ADD . /web
 
-RUN pip install --trusted-host pypi.python.org -r Requirements.txt
+RUN pip3 install -r Requirements.txt
 
-EXPOSE 443
+EXPOSE 80
+
+EXPOSE 22
 
 ENV NAME World
 
--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 
 from flask import Flask
-from redis import Redis, RedisError
+import subprocess
 import os
 import socket
 
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
+def command2exec():
+	output = subprocess.run(["ls", "-al", "/admin/"], stdout=subprocess.PIPE).stdout
+	return output
+
+def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+           "<b>Output:</b> {output} <br/>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(),output = command2exec())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443)
+    subprocess.run(["service", "ssh", "start"])
+    app.run( host='0.0.0.0', port=80)
 
